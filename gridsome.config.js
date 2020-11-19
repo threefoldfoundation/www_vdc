397a143bbaa03d4498d2d3cdc3e53b23cba204fb
sasha astiadi <sasha@incubaid.com>
2020-11-19 14:33:11 +0100

    ADD TOTLE

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'TFVDC',
+  siteName: 'TF',
   icon: {
     favicon: './src/assets/favicon.png',
     touchicon: './src/assets/favicon.png'
@@ -17,7 +17,7 @@ module.exports = {
     github: process.env.URL_GITHUB || false,
     nav: {
       links: [
-        { path: '/docs/', title: '' }
+        { path: '/docs/', title: 'VDC' }
       ]
     },
     sidebar: [
